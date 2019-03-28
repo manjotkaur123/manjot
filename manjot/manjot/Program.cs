b64565c782ac2800f4403ddf761d985d435de744
Manjot Kaur <731846@student.tor.loc>
2019-03-28 16:22:08 -0400

    manjot

---
+++ manjot/manjot/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace manjot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Peter Rules the IT Universe!");
+
+            Download();
+
+            Console.ReadLine();
+
+        }
+
+
+
+        static void Download()
+
+        {
+
+            Task.Run(() =>
+
+            {
+
+                Thread.Sleep(3000);
+
+                Console.WriteLine("Download Complete ...");
+
+            }
+
+                );
+
+        }
+
+    }
+
+}
